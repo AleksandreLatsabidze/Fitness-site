115eb70779cb090256d6e4d2f6b42ccff1342539
aleksandrelcb <121313546+AleksandreLatsabidze@users.noreply.github.com>
2024-08-01 00:45:09 +0400

    CrosstimeTable with endless js

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// Add Active Class to Current Element *************
+var ul = document.getElementById("myUL");
+var links = ul.getElementsByClassName("nav-link");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    for (var j = 0; j < links.length; j++) {
+      links[j].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
+//****************************************************
+// document.querySelectorAll(".filter-link").forEach((link) => {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     filterSelection(this.getAttribute("data-filter"));
+//   });
+// });
+
+// function filterSelection(filter) {
+//   var rows = document.querySelectorAll("#filterTable .filterDiv");
+//   rows.forEach((row) => {
+//     if (filter === "all" || row.classList.contains(filter)) {
+//       row.classList.remove("hidden");
+//     } else {
+//       row.classList.add("hidden");
+//     }
+//   });
+
+//   updateActiveLink(filter);
+// }
+
+// function updateActiveLink(filter) {
+//   document.querySelectorAll(".filter-link").forEach((link) => {
+//     link.classList.toggle(
+//       "active",
+//       link.getAttribute("data-filter") === filter
+//     );
+//   });
+// }
